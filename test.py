ac571bf553ea78b7192f06b2f61241e2b8defde3
michelle-2808 <wjones13522@gmail.com>
2025-03-16 21:32:16 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
 print("Successfully Deployed the Jenkins Email Notification Project")
 
 print("Now this will run by Automation using poll SCM after each 5 minutes")
+
+print("wrong syntax - no backet complete"
